8c1b5ff59b845c96d06e3db6b215f4f82929500f
janeeats <jane.vora@gmail.com>
2016-12-18 22:33:41 -0500

    dooooooone

--- app/helpers/wishes_helper.rb
+++ app/helpers/wishes_helper.rb
@@ -1,2 +1,29 @@
 module WishesHelper
+
+	def wish_page_class(index)
+	  if index.zero?
+	    "item active"
+	  else
+	  	"item"
+	  end
+	end
+
+	def wish_pagination_class(index)
+	  if index.zero?
+	    "active"
+	  else
+	  	""
+	  end
+	end
+
+	def wish_page_slice_count(wish_page)
+		if wish_page.count >= 9
+			3
+		elsif wish_page.count <= 3
+			1
+		else
+			2
+		end
+	end
+
 end
