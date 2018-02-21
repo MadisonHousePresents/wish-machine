70b26fe4d3c63572a8c8431e399de55669d43c94
janeeats <jane.vora@gmail.com>
2018-02-21 12:59:32 -0500

    switch routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   resources :granted_wishes
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-	root to: 'users#new'
+	root to: 'granted_wishes#index'
 
-  resources :wishes
-  resources :users
-  resources :granted_wishes
+  # resources :wishes
+  # resources :users
+  # resources :granted_wishes
 end
