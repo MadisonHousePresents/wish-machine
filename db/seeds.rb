6f0273199fbf5a70af09184e53ccbf45f04cf9d7
janeeats <jane.vora@gmail.com>
2018-11-12 10:51:49 -0500

    fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,168 +13,168 @@ users = User.create([
 		"phone": "123-456-7890",
 		"email": "lia@madisonhousepresents.com",
 		"terms_accepted": true,
+		"city": "Boulder",
+		"state": "Colorado",
 		"wish_attributes": {
 			"participant_requirement": "donate $1000 to the Electronic Frontier Foundation",
 			"ef_fulfillment": "Let me propose to my girlfriend on stage during flume",
-			"approved": true,
-			"city": "Boulder",
-			"state": "Colorado"
+			"approved": true
 		}
 	}, {
 		"name": "Lia Test Wisher",
 		"phone": "123-456-7890",
 		"email": "lia@madisonhousepresents.com",
 		"terms_accepted": true,
+		"city": "Boulder",
+		"state": "Colorado",
 		"wish_attributes": {
 			"participant_requirement": "memorize 30 poems about trees",
 			"ef_fulfillment": "let me be a forest character performer",
-			"approved": true,
-			"city": "Boulder",
-			"state": "Colorado"
+			"approved": true
 		}
 	}, {
 		"name": "Lia Test Wisher",
 		"phone": "123-456-7890",
 		"email": "lia@madisonhousepresents.com",
 		"terms_accepted": true,
+		"city": "Boulder",
+		"state": "Colorado",
 		"wish_attributes": {
 			"participant_requirement": "learn to walk again by May 1st",
 			"ef_fulfillment": "give me a free good life wristband for the second weekend",
-			"approved": true,
-			"city": "Boulder",
-			"state": "Colorado"
+			"approved": true
 		}
 	}, {
 		"name": "Lia Test Wisher",
 		"phone": "123-456-7890",
 		"email": "lia@madisonhousepresents.com",
 		"terms_accepted": true,
+		"city": "Boulder",
+		"state": "Colorado",
 		"wish_attributes": {
 			"participant_requirement": "raise $5000 for my local elementary school",
 			"ef_fulfillment": "add my band to the lineup",
-			"approved": true,
-			"city": "Boulder",
-			"state": "Colorado"
+			"approved": true
 		}
 	}, {
 		"name": "Lia Test Wisher",
 		"phone": "123-456-7890",
 		"email": "lia@madisonhousepresents.com",
 		"terms_accepted": true,
+		"city": "Boulder",
+		"state": "Colorado",
 		"wish_attributes": {
 			"participant_requirement": "donate $1000 to the world wildlife fund in the name of the Forest Family",
 			"ef_fulfillment": "Let me have one of the sold out first weekend maplewoods tickets",
-			"approved": true,
-			"city": "Boulder",
-			"state": "Colorado"
+			"approved": true
 		}
 	}, {
 		"name": "Lia Test Wisher",
 		"phone": "123-456-7890",
 		"email": "lia@madisonhousepresents.com",
 		"terms_accepted": true,
+		"city": "Boulder",
+		"state": "Colorado",
 		"wish_attributes": {
 			"participant_requirement": "run a marathon",
 			"ef_fulfillment": "let me meet BANKS",
-			"approved": true,
-			"city": "Boulder",
-			"state": "Colorado"
+			"approved": true
 		}
 	}, {
 		"name": "Lia Test Wisher",
 		"phone": "123-456-7890",
 		"email": "lia@madisonhousepresents.com",
 		"terms_accepted": true,
+		"city": "Boulder",
+		"state": "Colorado",
 		"wish_attributes": {
 			"participant_requirement": "hold a handstand for 5 minutes",
 			"ef_fulfillment": "let me teach a yoga class",
-			"approved": true,
-			"city": "Boulder",
-			"state": "Colorado"
+			"approved": true
 		}
 	}, {
 		"name": "Lia Test Wisher",
 		"phone": "123-456-7890",
 		"email": "lia@madisonhousepresents.com",
 		"terms_accepted": true,
+		"city": "Boulder",
+		"state": "Colorado",
 		"wish_attributes": {
 			"participant_requirement": "lose 25 lbs by June 1",
 			"ef_fulfillment": "send me on a shopping spree with clothing vendors",
-			"approved": true,
-			"city": "Boulder",
-			"state": "Colorado"
+			"approved": true
 		}
 	}, {
 		"name": "Lia Test Wisher",
 		"phone": "123-456-7890",
 		"email": "lia@madisonhousepresents.com",
 		"terms_accepted": true,
+		"city": "Boulder",
+		"state": "Colorado",
 		"wish_attributes": {
 			"participant_requirement": "volunteer 20 hours at an animal shelter",
 			"ef_fulfillment": "give me a free ticket to the second weekend",
-			"approved": true,
-			"city": "Boulder",
-			"state": "Colorado"
+			"approved": true
 		}
 	}, {
 		"name": "Lia Test Wisher",
 		"phone": "123-456-7890",
 		"email": "lia@madisonhousepresents.com",
 		"terms_accepted": true,
+		"city": "Boulder",
+		"state": "Colorado",
 		"wish_attributes": {
 			"participant_requirement": "dress up with all of my friends and flash mob a subway train",
 			"ef_fulfillment": "give us a group camp spot close to the festival entrance",
-			"approved": true,
-			"city": "Boulder",
-			"state": "Colorado"
+			"approved": true
 		}
 	}, {
 		"name": "Lia Test Wisher",
 		"phone": "123-456-7890",
 		"email": "lia@madisonhousepresents.com",
 		"terms_accepted": true,
+		"city": "Boulder",
+		"state": "Colorado",
 		"wish_attributes": {
 			"participant_requirement": "give away 200 forest care packages to forest family members",
 			"ef_fulfillment": "let me host my own mini event in the forest",
-			"approved": true,
-			"city": "Boulder",
-			"state": "Colorado"
+			"approved": true
 		}
 	}, {
 		"name": "Lia Test Wisher",
 		"phone": "123-456-7890",
 		"email": "lia@madisonhousepresents.com",
 		"terms_accepted": true,
+		"city": "Boulder",
+		"state": "Colorado",
 		"wish_attributes": {
 			"participant_requirement": "go clean up the beach next to my house and pick up every piece of trash",
 			"ef_fulfillment": "let me play a song on stage with Cheese",
-			"approved": true,
-			"city": "Boulder",
-			"state": "Colorado"
+			"approved": true
 		}
 	}, {
 		"name": "Lia Test Wisher",
 		"phone": "123-456-7890",
 		"email": "lia@madisonhousepresents.com",
 		"terms_accepted": true,
+		"city": "Boulder",
+		"state": "Colorado",
 		"wish_attributes": {
 			"participant_requirement": "die my hair olive and lime in Electric Forest colors and shave #HappyForest into the side of my head",
 			"ef_fulfillment": "let me go on stage with Above \u0026 Beyond for their performance",
-			"approved": true,
-			"city": "Boulder",
-			"state": "Colorado"
+			"approved": true
 		}
 	}, {
 		"name": "Lia Test Wisher",
 		"phone": "123-456-7890",
 		"email": "lia@madisonhousepresents.com",
 		"terms_accepted": true,
+		"city": "Boulder",
+		"state": "Colorado",
 		"wish_attributes": {
 			"participant_requirement": "wear a tutu everyday until EF and post it on Instagram tagged #HappyForestTutu",
 			"ef_fulfillment": "give me tickets to both weekends",
-			"approved": true,
-			"city": "Boulder",
-			"state": "Colorado"
+			"approved": true
 		}
 	}
 ])
